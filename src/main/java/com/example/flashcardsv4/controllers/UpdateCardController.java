28e94637d747576180c44ca63afb46be52e8802e
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-06-08 11:15:01 +0300

    with some posts (without correct redirect)

--- src/main/java/com/example/flashcardsv4/controllers/UpdateCardController.java
+++ src/main/java/com/example/flashcardsv4/controllers/UpdateCardController.java
@@ -1,4 +1,27 @@
 package com.example.flashcardsv4.controllers;
 
-public class UpdateCardController {
+import com.example.flashcardsv4.service.CardService;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class UpdateCardController extends HttpServlet {
+    CardService cardService;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext context=getServletContext();
+        cardService=(CardService) context.getAttribute("cardService");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long cardId=Long.parseLong(req.getParameter("cardId"));
+        cardService.updateMemorizingToTrueForTraining(cardId);
+        resp.sendRedirect(req.getContextPath());
+    }
 }
